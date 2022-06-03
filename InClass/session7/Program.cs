34ba5159cc7cd2b92b7b9102c205d7fc828ad597
pedramsaharkhiz <pedram_saharkhiz@yahoo.com>
2022-06-04 01:57:58 +0430

    first commit

---
+++ InClass/session7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace session7
+{
+    public class Rectangle:Shape{
+        public Rectangle(int x,int y):base(x,y){
+            
+        }
+        public override void CalArea()
+        {
+            base.CalArea();
+            System.Console.WriteLine(X*Y);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x=Convert.ToInt32(Console.ReadLine());
+            int y=Convert.ToInt32(Console.ReadLine());
+            Rectangle R=new Rectangle(x,y);
+            R.CalArea();
+        }
+    }
+}
+
