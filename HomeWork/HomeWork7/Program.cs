76b00652bcb38788b3f907c1c96e2abbcc791220
pedramsaharkhiz <pedram_saharkhiz@yahoo.com>
2022-06-14 19:44:34 +0430

    first commit

--- HomeWork/HomeWork7/Program.cs
+++ HomeWork/HomeWork7/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Globalization;
+using System;
 
 namespace HomeWork7
 {
@@ -12,15 +13,26 @@ namespace HomeWork7
         }
 
     }
-    internal class Program
+    public class Program
     {
+        public static string res="";
+        public static string rec(string s,int n){
+             
+            if(n!=-1){
+                res+=s[n];
+                return rec(s,n-1);
+            }else{
+            return res;
+            }
+            
+        }
        
         static void Main(string[] args)
         {
             System.Console.WriteLine("Please Enter a string !");
             string s=Console.ReadLine();
             System.Console.WriteLine("Here is Result : ");
-            System.Console.WriteLine(s.P());
+            System.Console.WriteLine(rec(s,s.Length-1));
             
         }
     }
